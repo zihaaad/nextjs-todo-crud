9058de75f296f5570d925aca71473f02aeab4fb3
Zihadul Islam <zynscriptofficial@gmail.com>
2023-10-28 16:33:56 +0600

    get, put, delete apicreated

--- app/api/topics/route.js
+++ app/api/topics/route.js
@@ -8,3 +8,16 @@ export async function POST(request) {
   await Topic.create({title, description});
   return NextResponse.json({message: "Topic Created"}, {status: 201});
 }
+
+export async function GET() {
+  await connectMongoDB();
+  const topics = await Topic.find();
+  return NextResponse.json({topics});
+}
+
+export async function DELETE(request) {
+  const id = request.nextUrl.searchParams.get("id");
+  await connectMongoDB();
+  await Topic.findByIdAndDelete(id);
+  return NextResponse.json({message: "Topic Deleted"}, {status: 200});
+}
